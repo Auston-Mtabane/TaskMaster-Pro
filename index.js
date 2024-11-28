82dadc4bff53de96ceec3852e5de7218fdfeb1fa
Auston Mtabane <mtabane.auston@gmail.com>
2024-11-28 12:45:29 +0200

    side bar 90% done

--- index.js
+++ index.js
@@ -1,7 +1,25 @@
 const button1 = document.getElementById('btn-1');
 const button2 = document.getElementById('btn-2');
 const listdiv = document.getElementById('listNameDiv');
+const listn = document.getElementsByClassName('list-name');
 let n_listname = 0;
+
+
+function handleDivClick(event) {
+   const allDivs = document.querySelectorAll('.list-name');
+  allDivs.forEach(div => {
+    div.contentEditable = 'True'; // Disable editing
+    div.classList.remove('selected'); // Remove selected class
+  });
+
+  // Select the clicked div
+  const listName = event.target;
+  listName.contentEditable = 'true'; // Enable editing for the clicked div
+  listName.classList.add('selected'); // Add selected class
+  console.log(listName.textContent)
+ }
+
+
 
 button1.addEventListener('click',function(){
    const listName = document.createElement('div');
@@ -9,9 +27,11 @@ button1.addEventListener('click',function(){
    n_listname++;
    listName.textContent = `list ${n_listname}`;
    listName.id = `L${n_listname}`
-   listName.contentEditable = 'true';
+   listName.contentEditable = 'True';
+   listName.addEventListener('click', handleDivClick);
 
-   listdiv.appendChild(listName)
+   listdiv.appendChild(listName);  
 
+   
 
 });
