542b3935f466aff63397673e0977098734ddcf98
Auston Mtabane <mtabane.auston@gmail.com>
2024-11-28 11:21:40 +0200

    sidebar 70% done

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 const button1 = document.getElementById('btn-1');
 const button2 = document.getElementById('btn-2');
+const listdiv = document.getElementById('listNameDiv');
 let n_listname = 0;
 
 button1.addEventListener('click',function(){
    const listName = document.createElement('div');
    listName.className = "list-name";
    n_listname++;
-   listName.textContent = `list ${n_listname}`
-   
+   listName.textContent = `list ${n_listname}`;
+   listName.id = `L${n_listname}`
+   listName.contentEditable = 'true';
 
+   listdiv.appendChild(listName)
 
 
 });
